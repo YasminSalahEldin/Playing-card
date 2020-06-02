567364b60b4addb58662a6d214716c7878760592
yasmin salah <50454254+YasminSalahEldin@users.noreply.github.com>
2020-06-02 08:56:22 +0200

    Update untitled1.dart

--- untitled1.dart
+++ untitled1.dart
@@ -1,28 +1,46 @@
 import 'dart:math';
 import 'dart:io';
-import '';
 void main(){
-
 // HEART , DIAMOND ,SPADE , CLUBS
 //1-10 -j , Q, K
   List<int> numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,];
   List<String> signs = ['Hearts','Diamonds','Clubs','Spade'];
 
   Random numberCollection = Random();
+  int numberIndex = numberCollection.nextInt(12);
+
+  String name;
+  switch(numberIndex){
+    case 0:
+      name='Ace';
+      break;
+    case 10:
+      name= 'jack';
+      break;
+    case 11:
+      name ='queen';
+      break;
+    case 12:
+      name = 'king';
+      break;
+    default:
+      name = numbers[numberIndex].toString();
+      break;
+  }
+
   Random signCollection = Random();
+  int signIndex = signCollection.nextInt(3);
 
 
   // map
-  Map choice = ['Number' : 12];
+  Map choice = { 'Number' : name, 'Sign': signs[signIndex]};
+
   String number = choice['Number'];
+  String sign = choice["Sign"];
 
-  String sign = choice['sign'];
+  print("your card is $number $sign");
 
-  print('your cars is' +);
-
-  print('Welcome');
-
-
+}
 
 
 
