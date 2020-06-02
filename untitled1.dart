d60e567a6ad87526fa67d8d1dd05d80c51da43aa
yasmin salah <50454254+YasminSalahEldin@users.noreply.github.com>
2020-06-02 04:19:04 +0200

    Add files via upload

---
+++ untitled1.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+import 'dart:io';
+import '';
+void main(){
+
+// HEART , DIAMOND ,SPADE , CLUBS
+//1-10 -j , Q, K
+  List<int> numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,];
+  List<String> signs = ['Hearts','Diamonds','Clubs','Spade'];
+
+  Random numberCollection = Random();
+  Random signCollection = Random();
+
+
+  // map
+  Map choice = ['Number' : 12];
+  String number = choice['Number'];
+
+  String sign = choice['sign'];
+
+  print('your cars is' +);
+
+  print('Welcome');
+
+
+
+
+
+
+
+
+}
+
+
